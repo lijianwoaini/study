11f0e15c95bb399462e5b8913d75b51e314a063f
lijian942330958 <46781779+lijianwoaini@users.noreply.github.com>
2020-01-02 19:47:08 +0800

    第三次上传，修改表格样式

--- ajax_delete/test/01.php
+++ ajax_delete/test/01.php
@@ -12,8 +12,22 @@ $arr=$dao->fetchAll($sql);
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <style>
+        table{
+            width: 400px;
+            background-color: red;
+        }
+        tr{
+            border: 1px solid black;
+        }
+        td{
+            border: 1px solid black;
+        }
+    </style>
 </head>
 <body>
+<div class="container">
 <table>
     <tr>
         <td><a href="insert.html">添加</a></td>
@@ -33,6 +47,7 @@ $arr=$dao->fetchAll($sql);
     </tr>
     <?php } ?>
 </table>
+</div>
 <script src="jquery.min.js"></script>
 <script>
     $(".btn").click(function () {
